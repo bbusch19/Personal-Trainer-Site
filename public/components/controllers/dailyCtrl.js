e279ece2c61d105027247d5238fc826f6ea90f06
Breiden Busch <breiden.busch@gmail.com>
2016-03-30 17:27:30 -0600

    Styling

--- public/components/controllers/dailyCtrl.js
+++ public/components/controllers/dailyCtrl.js
@@ -9,6 +9,8 @@ mainSvc.getDailys().then(function(response) {
   $scope.dailys = response.data;
 })
 
+
+
 $scope.postDaily = function(postBody) {
   mainSvc.postDaily(postBody);
   $('textarea').val('');
