5aab60baf4df71b9283ddb9e51fef624c43b2646
Breiden Busch <breiden.busch@gmail.com>
2016-04-29 00:14:41 -0600

    More Changes

--- server.js
+++ server.js
@@ -73,7 +73,10 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use(cors());
-app.use(session({secret: process.env.SESSIONSECRET, resave: false, saveUninitialized: false}));
+app.use(session({
+    secret: process.env.SESSIONSECRET,
+    resave: false,
+    saveUninitialized: false}));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
